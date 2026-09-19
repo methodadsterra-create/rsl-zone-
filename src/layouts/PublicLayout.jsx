d720c88a29936fbc6813e39026e65dd1348b823b
methodadsterra-create <methodadsterra@gmail.com>
2026-09-19 18:27:37 +0100

    Add files via upload

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import AdTag from '../components/AdTag';
 
 export default function PublicLayout() {
   return (
     <div className="site-shell">
+      <AdTag />
       <Header />
       <main id="main-content">
         <Outlet />
