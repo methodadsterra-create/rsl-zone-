59e8f7fb2157b6baed764b33c1f55040a88a8ca1
methodadsterra-create <methodadsterra@gmail.com>
2026-09-19 18:56:02 +0100

    Add files via upload

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import AdTag from '../components/AdTag';
+import PresenceTracker from '../components/PresenceTracker';
 
 export default function PublicLayout() {
   return (
     <div className="site-shell">
       <AdTag />
+      <PresenceTracker />
       <Header />
       <main id="main-content">
         <Outlet />
